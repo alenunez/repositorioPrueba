eb58b90f087f348ea0b448abf54440b8e5723cb2
alenunez097 <alenunez097@gmail.com>
2021-10-10 18:38:20 -0500

    Cambio por Alejandro

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -2,7 +2,6 @@ public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados() {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
-        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
 
         return miVertebrados;
     }
